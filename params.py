9a1c2895b10c765fcc6e24ec436e2aed6a52e604
mgrody1 <mgrody1@umbc.edu>
2024-11-16 15:09:32 -0500

    feat: read in json config in params

--- params.py
+++ params.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import pandas as pd
 import os
+import json
 
 ### Environmental Variabls
 
@@ -16,21 +17,6 @@ db_pass = os.getenv("DB_PASS")
 ### Local Paths
 survivor_wb_path = r"/home/mhgr/Downloads/survivoR.xlsx"
 
-# Sheets to Tables Mapping
-SHEET_TO_TABLE = {
-    "Advantage Details": "Advantages",
-    "Advantage Movement": "Advantage_Movement",
-    "Boot Mapping": "Vote_History",
-    "Castaway Details": "Castaways",
-    "Castaways": "Tribe_Memberships",
-    "Challenge Description": "Challenges",
-    "Challenge Results": "Challenge_Results",
-    "Vote History": "Vote_History",
-    "Episodes": "Episodes",
-    "Confessionals": "Confessionals",
-    "Jury Votes": "Jury_Votes",
-    "Season Summary": "Seasons",
-    "Tribe Mapping": "Tribe_Memberships",
-    "Screen Time": "Screen_Time",
-    "Episode Summary": "Episodes"
-}
+### DB Config
+with open("table_config.json", "r") as f:
+    table_config = json.load(f)
