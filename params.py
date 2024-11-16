6a51af34f8cd10dc91681e023319579c06a4dc4b
mgrody1 <mgrody1@umbc.edu>
2024-11-16 13:31:45 -0500

    feat: add more vars to params

--- params.py
+++ params.py
@@ -15,3 +15,22 @@ db_pass = os.getenv("DB_PASS")
 
 ### Local Paths
 survivor_wb_path = r"/home/mhgr/Downloads/survivoR.xlsx"
+
+# Sheets to Tables Mapping
+SHEET_TO_TABLE = {
+    "Advantage Details": "Advantages",
+    "Advantage Movement": "Advantage_Movement",
+    "Boot Mapping": "Vote_History",
+    "Castaway Details": "Castaways",
+    "Castaways": "Tribe_Memberships",
+    "Challenge Description": "Challenges",
+    "Challenge Results": "Challenge_Results",
+    "Vote History": "Vote_History",
+    "Episodes": "Episodes",
+    "Confessionals": "Confessionals",
+    "Jury Votes": "Jury_Votes",
+    "Season Summary": "Seasons",
+    "Tribe Mapping": "Tribe_Memberships",
+    "Screen Time": "Screen_Time",
+    "Episode Summary": "Episodes"
+}
